96e6d6f64f403fbe35439b1b883cde32d560b79a
tmKcode <tz429707@students.mimuw.edu.pl>
2021-12-02 21:05:38 +0100

    Update clion utils

--- cmake-build-debug/CMakeFiles/zadanie4JNP.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/zadanie4JNP.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/jk/Desktop/zadanie4JNP/main.cpp" "/Users/jk/Desktop/zadanie4JNP/cmake-build-debug/CMakeFiles/zadanie4JNP.dir/main.cpp.o"
+  "/home/tmk/JNP/zad4/Zadanie4JNP/main.cpp" "/home/tmk/JNP/zad4/Zadanie4JNP/cmake-build-debug/CMakeFiles/zadanie4JNP.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
