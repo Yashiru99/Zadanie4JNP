0e006400b12293ee93764b4efb53d6aed15a9648
Julian Kozłowski <jk417694@students.mimuw.edu.pl>
2021-12-03 11:01:28 +0100

    pierwszy concept

--- treasure_hunt.h
+++ treasure_hunt.h
@@ -1,6 +1,33 @@
 #ifndef TREASURE_HUNT_H
 #define TREASURE_HUNT_H
 
-// należy zdefiniować concept encounterSide
+#include "treasure.h"
 
-#endif TREASURE_HUNT_H
+// poprawna reprezentacja szablonu treasure
+template<typename T>
+struct isTreasureStruct : std::false_type{};
+
+template<typename T, bool val>
+struct isTreasureStruct<Treasure<T, val>> : std::true_type{};
+
+template<typename T>
+concept isTreasure = isTreasureStruct<T>().operator bool();
+
+// poprawna reprezentacja szablonu uczestnika
+template<typename T>
+concept isAdventurer = requires (T v){
+    {v.strength_t};
+    {v.isArmed} -> std::convertible_to<bool>;
+    {v.pay()} -> TreasureValid;
+    // zostało jeszcze v.loot(treasure)
+};
+
+template<typename T>
+concept EncounterSide = isTreasure<T> || isAdventurer<T>;
+
+/* template<EncounterSide sideA, EncounterSide sideB>
+class Encounter{
+    run(encounter);
+} */
+
+#endif //TREASURE_HUNT_H
